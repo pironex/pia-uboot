0b7114f798619622d308aa7ef52054f606571f57
Björn Krombholz <b.krombholz@pironex.de>
2013-01-10 11:30:42 +0100

    pia-am335x: update mmc pinmux for gpios
    Signed-off-by: Björn Krombholz <b.krombholz@pironex.de>

--- board/ti/am335x/mux.c
+++ board/ti/am335x/mux.c
@@ -264,10 +264,9 @@ static struct module_pin_mux mmc0_pin_mux[] = {
 	{OFFSET(mmc0_dat2), (MODE(0) | RXACTIVE | PULLUP_EN)},	/* MMC0_DAT2 */
 	{OFFSET(mmc0_dat1), (MODE(0) | RXACTIVE | PULLUP_EN)},	/* MMC0_DAT1 */
 	{OFFSET(mmc0_dat0), (MODE(0) | RXACTIVE | PULLUP_EN)},	/* MMC0_DAT0 */
-	{OFFSET(mmc0_clk), (MODE(0) | RXACTIVE | PULLUP_EN)},	/* MMC0_CLK */
-	{OFFSET(mmc0_cmd), (MODE(0) | RXACTIVE | PULLUP_EN)},	/* MMC0_CMD */
-	{OFFSET(mcasp0_aclkr), (MODE(4) | RXACTIVE)},		/* MMC0_WP */
-	{OFFSET(spi0_cs1), (MODE(5) | RXACTIVE | PULLUP_EN)},	/* MMC0_CD */
+	{OFFSET(mmc0_clk),  (MODE(0) | RXACTIVE | PULLUP_EN)},	/* MMC0_CLK */
+	{OFFSET(mmc0_cmd),  (MODE(0) | RXACTIVE | PULLUP_EN)},	/* MMC0_CMD */
+	{OFFSET(mii1_txd2), (MODE(7) | RXACTIVE | PULLUP_EN)},	/* MMC0_CD */
 	{-1},
 };
 #endif
