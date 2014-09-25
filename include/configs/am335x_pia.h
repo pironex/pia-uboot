bc77cee4ec34f40d3c8bb45619ec312ec9d826f8
Björn Krombholz <b.krombholz@pironex.de>
2014-09-25 15:43:54 +0200

    pia-am335x-ebtft: add rev 0.02
    Signed-off-by: Björn Krombholz <b.krombholz@pironex.de>

--- include/configs/am335x_pia.h
+++ include/configs/am335x_pia.h
@@ -44,7 +44,7 @@
   #define CONFIG_BOARD_REVISION "0.02"
   #if defined(CONFIG_PIA_EBTFT)
    #define CONFIG_EXP_NAME "P335BEBT"
-   #define CONFIG_EXP_REV "0.01"
+   #define CONFIG_EXP_REV "0.02"
   #elif defined(CONFIG_PIA_APC)
    #define CONFIG_EXP_NAME "P335BAPC"
    #define CONFIG_EXP_REV "0.01"
