aeb7e538a1b4f9e349cb100f5ba4124fafaf99ee
Björn Krombholz <b.krombholz@pironex.de>
2015-01-13 18:09:48 +0100

    pia-am35x: add support for J043 display expansion
    Signed-off-by: Björn Krombholz <b.krombholz@pironex.de>

--- board/ti/am35xpia/am35xpia.c
+++ board/ti/am35xpia/am35xpia.c
@@ -67,7 +67,8 @@
 #define PIA_LCD        0x00010005	//LCD Board
 #define PIA_LCD_DEM    0x00010006	//LCD-Board with DEM 480272D display
 #define PIA_LCD_DT028  0x00010007	//LCD-Board with Displaytech DT028ATFT
-#define PIA_MESS 	   0x00010008	//Measurement Amplifier
+#define PIA_MESS       0x00010008	//Measurement Amplifier
+#define PIA_LCD_J043   0x00010009	//LCD-Board with J043
 #define PIA_EMS_IO     0x00020001   //special EMS IO Expander
 #define PIA_NEW_EEPROM 0xffffffff
 #define PIA_NO_EEPROM  0x00000000
@@ -335,6 +336,12 @@ int misc_init_r(void)
 		else
 			setenv("buddy_lcd", "pia_lcd_dt028 omapfb.vrfb=y omapfb.rotate=1");
 		setenv("display","lcd");
+		break;
+	case PIA_LCD_J043:
+		printf("LCD: piA-LCD_J043 (rev %d)\n", expansion_config.revision);
+		MUX_PIA_LCD();
+		setenv("buddy_lcd", "pia_lcd_j043");
+		setenv("display", "lcd");
 		break;
 	case PIA_MESS:
 		printf("Expansion Board: piA-MESS (rev %d)\n", expansion_config.revision);
